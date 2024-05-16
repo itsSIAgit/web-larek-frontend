853a90d6b6ca58ee74704e9567dbe98973b3db45
Ilya Solovev <srpanmf@yandex.ru>
2024-05-17 07:45:28 +1000

    Документация и типы, на ревью

---
+++ src/types/index.ts
@@ -0,0 +1,75 @@
+import { IEvents } from "../components/base/events";
+
+//Интерфейс данных одного продукта
+export interface IProduct {
+  id: string;
+  description: string;
+  image: string;
+  title: string;
+  category: string;
+  price: number | null;
+}
+
+//Интерфейс каталога товаров
+export interface Catalog {
+  events: IEvents;
+  items: IProduct[];
+  setGoods (items: IProduct[]): void;
+  getProduct (id: string): IProduct;
+}
+
+//Интерфейс данных корзины
+export interface IBasket {
+  events: IEvents;
+  items: string[];
+  total: number | null;
+  add (id: string): void;
+  remove (id: string): void;
+  purchaseOpportunity (): boolean;
+  haveProduct (id: string): boolean;
+  goodsCount (): number;
+}
+
+//Интерфейс данных для оформления покупки
+export interface IPurchaseInfo {
+  events: IEvents;
+  payment: null | 'online' | 'physically';
+  address: string;
+  email: string;
+  phone: string;
+}
+
+//Интерфейс всплывающего окна (компонент Popup)
+interface IPopup {
+  content: HTMLElement;
+  open(): void;
+  close(): void;
+  erase (): void;
+}
+
+//Типы для работы с Api (компонент ShopApi)
+type TApiPostMethods = 'POST';
+
+//Тип данных с сервера после успешной покупки
+type TApiPostResult = {
+  id: string;
+  total: number;
+}
+
+//Тип данных отправляемых на сервер при покупке
+export type TApiPostData = {
+  payment: 'online' | 'physically';
+  email: string;
+  phone: string;
+  address: string;
+  total: number;
+  items: string[];
+}
+
+//Интерфейс для работы с Api
+//(отдельно чтобы не расширять базовый и не зависеть от него)
+export interface IApi {
+    baseUrl: string;
+    get<T>(uri: string): Promise<T>;
+    post<T>(uri: string, data: object, method?: TApiPostMethods): Promise<T>;
+}
